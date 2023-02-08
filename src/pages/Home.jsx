b26914cb78568c3ff7efa1429a8f0e853ad20cc5
Michael Pingleton <mnpingleton@gmail.com>
2023-02-08 17:47:37 -0500

    Added badges for technologies on the homepage.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Panel from '../components/Panel';
+import Badge from '../components/Badge';
 
 function Home() {
     return (
@@ -15,6 +16,36 @@ function Home() {
                                 My name is Michael Pingleton.  I am a software developer who is well versed in web, desktop, and mobile development alike.
                                 I am familiar with several different programming languages, technologies, and platforms:
                         </p>
+                        <div className="homepage_badges_wrapper">
+                            <div className="homepage_badges">
+                                <Badge badgeColor="black" textColor="lightgray" text="C" />
+                                <Badge badgeColor="black" textColor="lightgray" text="C++" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Java" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Kotlin" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Swift" />
+                                <Badge badgeColor="black" textColor="lightgray" text="JavaScript" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Python" />
+                                <Badge badgeColor="black" textColor="lightgray" text="HTML" />
+                                <Badge badgeColor="black" textColor="lightgray" text="CSS" />
+                            </div>
+                            <div className="homepage_badges">
+                                <Badge badgeColor="black" textColor="lightgray" text="React" />
+                                <Badge badgeColor="black" textColor="lightgray" text="NodeJS" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Vulkan" />
+                                <Badge badgeColor="black" textColor="lightgray" text="OpenGL" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Sequelize" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Prisma ORM" />
+                                <Badge badgeColor="black" textColor="lightgray" text="ExpressJS" />
+                            </div>
+                            <div className="homepage_badges">
+                                <Badge badgeColor="black" textColor="lightgray" text="iOS" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Android" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Windows" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Mac" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Linux" />
+                                <Badge badgeColor="black" textColor="lightgray" text="Web" />
+                            </div>
+                        </div>
                         <p>
                             On this site, you will be able to see some of the projects that I am working on by clicking the 'Projects' tab.
                             Although I am not looking for employment or collaboration opportunities at the moment, you can send me a message by clicking the 'Contact' tab.
