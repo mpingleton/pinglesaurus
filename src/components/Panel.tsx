68fd510864e622f0af70ff3095f335d6837ba66f
Michael Pingleton <mnpingleton@gmail.com>
2024-07-20 13:17:46 -0400

    Converted Panel to Typescript.

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react"
 
-function Panel(props) {
+function Panel(props: { isMobile: boolean, children: React.ReactElement }) {
     return (
         <div className={props.isMobile === true ? "mobile_panel" : "panel"}>
             {props.children}
         </div>
-    );
+    )
 }
 
-export default Panel;
+export default Panel
