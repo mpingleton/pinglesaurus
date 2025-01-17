e993392f9acdf70fb18890b3596c4510b425d345
Michael Pingleton <mnpingleton@gmail.com>
2025-01-17 14:40:37 -0500

    Image component now has the ability to either be bordered or un bordered.

--- src/components/atomic/Image.tsx
+++ src/components/atomic/Image.tsx
@@ -3,6 +3,7 @@ import React from "react"
 export default function Image(props: {
     width?: string,
     height?: string,
+    bordered?: boolean,
     url: string,
     alt: string
 }) {
@@ -11,6 +12,10 @@ export default function Image(props: {
         display: "block",
         width: props.width === undefined ? "auto" : props.width!,
         height: props.height === undefined ? "auto" : props.height!,
+        borderWidth: props.bordered ? "1px" : "0px",
+        borderColor: props.bordered ? "gray" : "transparent",
+        borderRadius: props.bordered ? "5px" : "0px",
+        borderStyle: props.bordered ? "solid" : "none"
     }
 
     return (<img style={sx} src={props.url} alt={props.alt} />)
