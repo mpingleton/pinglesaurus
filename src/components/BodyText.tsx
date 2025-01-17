0020cdb0252caa105e96466ee83158ff237a1cc2
Michael Pingleton <mnpingleton@gmail.com>
2025-01-17 12:13:59 -0500

    Added text components which don't depend on the CSS file.

--- src/components/BodyText.tsx
+++ src/components/BodyText.tsx
@@ -1,13 +1,26 @@
 import React from "react"
+import { Property } from "csstype"
 
-export default function BodyText(props: { children?: React.ReactNode }) {
+export default function BodyText(props: {
+    centered?: boolean,
+    children?: React.ReactNode
+}) {
+
+    let textAlign: Property.TextAlign = "left"
+    if (props.centered !== undefined) {
+        if (props.centered) {
+            textAlign = "center"
+        }
+    }
 
     const st: React.CSSProperties = {
         fontFamily: "Arial, sans-serif",
         fontSize: "14pt",
         fontWeight: "normal",
         margin: "0px",
-        padding: "0px"
+        padding: "0px",
+        width: "100%",
+        textAlign
     }
 
     return (<p style={st}>{props.children}</p>)
--- src/components/TitleText.tsx
+++ src/components/TitleText.tsx
@@ -1,13 +1,26 @@
 import React from "react"
+import { Property } from "csstype"
 
-export default function HeaderText(props: { children?: React.ReactNode }) {
+export default function HeaderText(props: {
+    centered?: boolean,
+    children?: React.ReactNode
+}) {
+
+    let textAlign: Property.TextAlign = "left"
+    if (props.centered !== undefined) {
+        if (props.centered) {
+            textAlign = "center"
+        }
+    }
 
     const st: React.CSSProperties = {
         fontFamily: "Arial, sans-serif",
         fontSize: "28pt",
         fontWeight: "bold",
         margin: "0px",
-        padding: "0px"
+        padding: "0px",
+        width: "100%",
+        textAlign
     }
 
     return (<h1 style={st}>{props.children}</h1>)
