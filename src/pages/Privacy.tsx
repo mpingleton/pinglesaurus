a81877fd0d4b9e5470f6f6bd7bcb9bda010420fc
Michael Pingleton <mnpingleton@gmail.com>
2025-01-17 14:32:17 -0500

    Migrated Privacy page new components.

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,68 +1,34 @@
 import React from "react"
-import Panel from "../components/Panel"
+
+import VStack from "../components/atomic/VStack"
+import TitlePanel from "../components/molecular/TitlePanel"
+import HeaderContentPanel from "../components/molecular/HeaderContentPanel"
 
 export default function Privacy(props: { isMobile: boolean }) {
 
-    const titlePanel = (
-        <Panel isMobile={props.isMobile}>
-            <center><h1>Privacy Policy</h1></center>
-        </Panel>
-    )
+    const titleIntroduction = "Introduction"
+    const contentIntroduction = "The purpose of this privacy policy is to inform you of the way your data is collected, processed, and stored while using the pinglesaur.us website. It is important to employ responsible and ethical practices when handling user data."
 
-    const introductionPanel = (
-        <Panel isMobile={props.isMobile}>
-            <center><h2>Introduction</h2></center>
-            <p>
-                The purpose of this privacy policy is to inform you of the way your data is collected, processed, and stored while using the pinglesaur.us website.
-                It is important to employ responsible and ethical practices when handling user data.
-            </p>
-        </Panel>
-    )
+    const p1Title = "Information You Give"
+    const p1Content = "The information you may give includes the email address that you may optionally provide when using the contact form."
 
-    const p1Panel = (
-        <Panel isMobile={props.isMobile}>
-            <center><h2>Information You Give</h2></center>
-            <p>
-                The information you may give includes the email address that you may optionally provide when using the contact form.
-            </p>
-        </Panel>
-    )
+    const p2Title = "Logging"
+    const p2Content = "Device data, such as IP addresses, may be logged upon accessing pinglesaur.us servers."
 
-    const p2Panel = (
-        <Panel isMobile={props.isMobile}>
-            <center><h2>Logging</h2></center>
-            <p>
-                Device data, such as IP addresses, may be logged upon accessing pinglesaur.us servers.
-            </p>
-        </Panel>
-    )
+    const p3Title = "Third Parties"
+    const p3Content = "Third-party servers are used to serve web content, as well as collect, process, and store data."
 
-    const p3Panel = (
-        <Panel isMobile={props.isMobile}>
-            <center><h2>Third Parties</h2></center>
-            <p>
-                Third-party servers are used to serve web content, as well as collect, process, and store data.
-            </p>
-        </Panel>
-    )
-
-    const p4Panel = (
-        <Panel isMobile={props.isMobile}>
-            <center><h2>Cookies</h2></center>
-            <p>
-                The pinglesaur.us website does not make use of cookies.
-            </p>
-        </Panel>
-    )
+    const p4Title = "Cookies"
+    const p4Content = "The pinglesaur.us website does not make use of cookies."
 
     return (
-        <div className="privacypage">
-            {titlePanel}
-            {introductionPanel}
-            {p1Panel}
-            {p2Panel}
-            {p3Panel}
-            {p4Panel}
-        </div>
-    );
+        <VStack>
+            <TitlePanel title="Privacy Policy" />
+            <HeaderContentPanel header={titleIntroduction} content={contentIntroduction} />
+            <HeaderContentPanel header={p1Title} content={p1Content} />
+            <HeaderContentPanel header={p2Title} content={p2Content} />
+            <HeaderContentPanel header={p3Title} content={p3Content} />
+            <HeaderContentPanel header={p4Title} content={p4Content} />
+        </VStack>
+    )
 }
