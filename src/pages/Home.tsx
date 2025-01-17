d477c10e3ee5153c07404d20241dda80227036fa
Michael Pingleton <mnpingleton@gmail.com>
2025-01-17 14:04:41 -0500

    Migrated Home page to new components.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,86 +1,70 @@
 import React from "react"
-import Panel from "../components/Panel"
-import Badge from "../components/Badge"
-import TitleText from "../components/TitleText"
-import HeaderText from "../components/HeaderText"
-import BodyText from "../components/BodyText"
-import VStack from "../components/VStack"
-import HStack from "../components/HStack"
+
+import TitleText from "../components/atomic/TitleText"
+import BodyText from "../components/atomic/BodyText"
+import VStack from "../components/atomic/VStack"
+import HStack from "../components/atomic/HStack"
+import Panel from "../components/atomic/Panel"
+import Badge from "../components/atomic/Badge"
+
+import XStack from "../components/molecular/XStack"
+import ProfileImage from "../components/molecular/ProfileImage"
 
 export default function Home(props: { isMobile: boolean }) {
 
-    const homepagePicture = (
-        <div id="homepage_picture">
-            <img
-                id={props.isMobile === true ? "homepage_mobile_pinglesaurus" : "homepage_pinglesaurus"}
-                src="/images/pinglesaurus.png"
-                alt="Pinglesaurus"
-            />
-        </div>
-    )
-
     const badgeContent = (
-        <div className={props.isMobile === true ? "homepage_mobile_badges_wrapper" : "homepage_badges_wrapper"}>
-            <div className={props.isMobile === true ? "homepage_mobile_badges" : "homepage_badges"}>
-                <Badge badgeColor="black" textColor="lightgray" text="C"/>
-                <Badge badgeColor="black" textColor="lightgray" text="C++"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Java"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Kotlin"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Swift"/>
-                <Badge badgeColor="black" textColor="lightgray" text="JavaScript"/>
-                <Badge badgeColor="black" textColor="lightgray" text="TypeScript"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Python"/>
-                <Badge badgeColor="black" textColor="lightgray" text="HTML"/>
-                <Badge badgeColor="black" textColor="lightgray" text="CSS"/>
-            </div>
-            <div className={props.isMobile === true ? "homepage_mobile_badges" : "homepage_badges"}>
-                <Badge badgeColor="black" textColor="lightgray" text="React"/>
-                <Badge badgeColor="black" textColor="lightgray" text="NodeJS"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Vulkan"/>
-                <Badge badgeColor="black" textColor="lightgray" text="OpenGL"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Sequelize"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Prisma ORM"/>
-                <Badge badgeColor="black" textColor="lightgray" text="ExpressJS"/>
-                <Badge badgeColor="black" textColor="lightgray" text="CUDA"/>
-            </div>
-            <div className={props.isMobile === true ? "homepage_mobile_badges" : "homepage_badges"}>
-                <Badge badgeColor="black" textColor="lightgray" text="iOS"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Android"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Windows"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Mac"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Linux"/>
-                <Badge badgeColor="black" textColor="lightgray" text="Web"/>
-            </div>
-        </div>
+        <VStack width="100%" gapping="2px">
+            <HStack width="100%" justifyItems="center" justifyContent="center" padding="0px" gapping="2px">
+                <Badge badgeColor="black" textColor="lightgray" text="C" />
+                <Badge badgeColor="black" textColor="lightgray" text="C++" />
+                <Badge badgeColor="black" textColor="lightgray" text="Java" />
+                <Badge badgeColor="black" textColor="lightgray" text="Kotlin" />
+                <Badge badgeColor="black" textColor="lightgray" text="Swift" />
+                <Badge badgeColor="black" textColor="lightgray" text="JavaScript" />
+                <Badge badgeColor="black" textColor="lightgray" text="TypeScript" />
+                <Badge badgeColor="black" textColor="lightgray" text="Python" />
+                <Badge badgeColor="black" textColor="lightgray" text="HTML" />
+                <Badge badgeColor="black" textColor="lightgray" text="CSS" />
+            </HStack>
+            <HStack width="100%" justifyItems="center" justifyContent="center" padding="0px" gapping="2px">
+                <Badge badgeColor="black" textColor="lightgray" text="React" />
+                <Badge badgeColor="black" textColor="lightgray" text="NodeJS" />
+                <Badge badgeColor="black" textColor="lightgray" text="Vulkan" />
+                <Badge badgeColor="black" textColor="lightgray" text="OpenGL" />
+                <Badge badgeColor="black" textColor="lightgray" text="Sequelize" />
+                <Badge badgeColor="black" textColor="lightgray" text="Prisma ORM" />
+                <Badge badgeColor="black" textColor="lightgray" text="ExpressJS" />
+                <Badge badgeColor="black" textColor="lightgray" text="CUDA" />
+            </HStack>
+            <HStack width="100%" justifyItems="center" justifyContent="center" padding="0px" gapping="2px">
+                <Badge badgeColor="black" textColor="lightgray" text="iOS" />
+                <Badge badgeColor="black" textColor="lightgray" text="Android" />
+                <Badge badgeColor="black" textColor="lightgray" text="Windows" />
+                <Badge badgeColor="black" textColor="lightgray" text="Mac" />
+                <Badge badgeColor="black" textColor="lightgray" text="Linux" />
+                <Badge badgeColor="black" textColor="lightgray" text="Web" />
+            </HStack>
+        </VStack>
     )
 
     const homepageContent = (
-        <div id="homepage_content">
-            <center>
-                <TitleText>This is some title text!</TitleText>
-                <HeaderText>This is some header text!</HeaderText>
-                <BodyText>Here is some body text!</BodyText>
-                <h1>Hello!</h1>
-                <p>
-                    My name is Michael Pingleton. I am a software developer who is well versed in web, desktop, and mobile development alike.
-                    I am familiar with several different programming languages, technologies, and platforms:
-                </p>
-                {badgeContent}
-                <p>
-                    On this site, you will be able to see some of the projects that I am working on by clicking the 'Projects' tab.
-                </p>
-            </center>
-        </div>
+        <VStack justifyItems="center" width="100%" gapping="14px">
+            <TitleText centered>Hello!</TitleText>
+            <BodyText centered>My name is Michael Pingleton. I am a software developer who is well versed in web, desktop, and mobile development alike.</BodyText>
+            <BodyText centered>I am familiar with several different programming languages, technologies, and platforms:</BodyText>
+            {badgeContent}
+            <BodyText centered>On this site, you will be able to see some of the projects that I am working on by clicking the 'Projects' tab.</BodyText>
+        </VStack>
     )
 
     return (
-        <div>
-            <Panel isMobile={props.isMobile}>
-                <div id={props.isMobile === true ? "homepage_mobile_wrapper" : "homepage_wrapper"}>
-                    {homepagePicture}
+        <VStack>
+            <Panel>
+                <XStack vertical={props.isMobile} horizontal={!props.isMobile}>
+                    <ProfileImage isMobile={props.isMobile} />
                     {homepageContent}
-                </div>
+                </XStack>
             </Panel>
-        </div>
+        </VStack>
     );
 }
