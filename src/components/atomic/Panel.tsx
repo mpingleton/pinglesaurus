6a474f963954daadd39c51ee2b9c2dbfee3a1bba
Michael Pingleton <mnpingleton@gmail.com>
2025-01-17 14:01:16 -0500

    Revamped Panel component.

---
+++ src/components/atomic/Panel.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Panel(props: { children: React.ReactNode }) {
+
+    const st: React.CSSProperties = {
+        display: "block",
+        boxSizing: "border-box",
+        width: "100%",
+        margin: "0px",
+        borderWidth: "1px",
+        borderColor: "gray",
+        borderStyle: "solid",
+        borderRadius: "10px",
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+        backdropFilter: "blur(8px)",
+        WebkitBackdropFilter: "blur(8px)"
+    }
+
+    return (<div style={st}>{props.children}</div>)
+}
