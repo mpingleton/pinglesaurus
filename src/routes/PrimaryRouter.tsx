2b2f67599cabe4e0229cb9ff5fcb2b8d8e986f8e
Michael Pingleton <mnpingleton@gmail.com>
2025-01-18 07:04:44 -0500

    Got rid of the old Projects page.  We're starting over with that.

--- src/routes/PrimaryRouter.tsx
+++ src/routes/PrimaryRouter.tsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MainLayout from "../layouts/MainLayout"
 import MobileLayout from "../layouts/MobileLayout"
 import Home from "../pages/Home"
-import ProjectProfile from "../pages/ProjectProfile"
-import Projects from "../pages/Projects"
 import Privacy from "../pages/Privacy"
 
 export default function PrimaryRouter() {
@@ -17,28 +15,12 @@ export default function PrimaryRouter() {
                     element={<MainLayout title="Home"><Home isMobile={false} /></MainLayout>}
                 />
                 <Route
-                    path="/projects/id/:projectId"
-                    element={<MainLayout title="Project"><ProjectProfile isMobile={false} /></MainLayout>}
-                />
-                <Route
-                    path="/projects"
-                    element={<MainLayout title="Projects"><Projects isMobile={false} /></MainLayout>}
-                />
-                <Route
                     path="/privacy"
                     element={<MainLayout title="Privacy Policy"><Privacy isMobile={false} /></MainLayout>}
                 />
                 <Route
                     path="/mobile"
                     element={<MobileLayout title="Home"><Home isMobile={true} /></MobileLayout>}
-                />
-                <Route
-                    path="/mobile/projects/id/:projectId"
-                    element={<MobileLayout title="Project"><ProjectProfile isMobile={true} /></MobileLayout>}
-                />
-                <Route
-                    path="/mobile/projects"
-                    element={<MobileLayout title="Projects"><Projects isMobile={true} /></MobileLayout>}
                 />
                 <Route
                     path="/mobile/privacy"
