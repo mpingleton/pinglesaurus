6e08be211e64f323b660fafe69ba588ae0c99a65
Michael Pingleton <mnpingleton@gmail.com>
2025-01-18 12:28:45 -0500

    Added MobileAppBar and MobileAppBarButton components.

---
+++ src/components/atomic/MobileAppBarButton.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+export default function MobileAppBarButton(props: {
+    title: string,
+    highlighted?: boolean,
+    href?: string,
+    onClicc?: () => void
+}) {
+
+    function handleClick() {
+        if (props.onClicc) {
+            props.onClicc()
+        }
+    }
+
+    const stLi: React.CSSProperties = {
+        display: "inline",
+        width: "100%",
+        height: "100%"
+    }
+
+    const stA: React.CSSProperties = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        textDecoration: "none",
+        fontSize: "20pt",
+        color: "lightgray"
+    }
+
+    return (
+        <li style={stLi}>
+            <a style={stA} href={props.href} onClick={handleClick}>{props.title}</a>
+        </li>
+    )
+}
