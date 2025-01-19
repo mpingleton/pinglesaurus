41bbdf4fb424aee15cb7eb0d29c49a5fd0015a9c
Michael Pingleton <mnpingleton@gmail.com>
2025-01-19 13:10:57 -0500

    Added SoftwareProject class.

--- src/classes/DataContext.ts
+++ src/classes/DataContext.ts
@@ -1,10 +1,12 @@
 import Language from "./Language"
 import Platform from "./Platform"
 import Framework from "./Framework"
+import SoftwareProject from "./SoftwareProject"
 
 export default class DataContext {
     isInitialized: boolean = false
     languages?: Language[]
     platforms?: Platform[]
     frameworks?: Framework[]
+    software?: SoftwareProject[]
 }
