f063059ceb48a1a9774ea92dcb6172899ee7b2ec
Michael Pingleton <mnpingleton@gmail.com>
2025-01-18 19:41:01 -0500

    Saving the current progress of the home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,75 +1,157 @@
 import React from "react"
-import { isBrowser, isMobile } from "react-device-detect"
-
-import TitleText from "../components/atomic/TitleText"
-import BodyText from "../components/atomic/BodyText"
-import VStack from "../components/atomic/VStack"
-import HStack from "../components/atomic/HStack"
-import Panel from "../components/atomic/Panel"
-import Badge from "../components/atomic/Badge"
-
-import XStack from "../components/molecular/XStack"
-import ProfileImage from "../components/molecular/ProfileImage"
+import { BrowserView, MobileView } from "react-device-detect"
 
 import AutomaticLayout from "../layouts/AutomaticLayout"
 
+import Box from "../components/atomic/Box"
+import VStack from "../components/atomic/VStack"
+import HStack from "../components/atomic/HStack"
+import HDivider from "../components/atomic/HDivider"
+import Panel from "../components/atomic/Panel"
+import Badge from "../components/atomic/Badge"
+import TitleText from "../components/atomic/TitleText"
+import BodyText from "../components/atomic/BodyText"
+import Image from "../components/atomic/Image"
+
+import ProfileImage from "../components/molecular/ProfileImage"
+import SectionPanel from "../components/molecular/SectionPanel"
+import FooterPanel from "../components/molecular/FooterPanel"
+import SocialMediaPanelButton from "../components/molecular/SocialMediaPanelButton"
+
 export default function Home() {
 
-    const badgeContent = (
-        <VStack width="100%" gapping="2px">
-            <HStack width="100%" justifyItems="center" justifyContent="center" padding="0px" gapping="2px">
-                <Badge badgeColor="black" textColor="lightgray" text="C" />
-                <Badge badgeColor="black" textColor="lightgray" text="C++" />
-                <Badge badgeColor="black" textColor="lightgray" text="Java" />
-                <Badge badgeColor="black" textColor="lightgray" text="Kotlin" />
-                <Badge badgeColor="black" textColor="lightgray" text="Swift" />
-                <Badge badgeColor="black" textColor="lightgray" text="JavaScript" />
-                <Badge badgeColor="black" textColor="lightgray" text="TypeScript" />
-                <Badge badgeColor="black" textColor="lightgray" text="Python" />
-                <Badge badgeColor="black" textColor="lightgray" text="HTML" />
-                <Badge badgeColor="black" textColor="lightgray" text="CSS" />
-            </HStack>
-            <HStack width="100%" justifyItems="center" justifyContent="center" padding="0px" gapping="2px">
-                <Badge badgeColor="black" textColor="lightgray" text="React" />
-                <Badge badgeColor="black" textColor="lightgray" text="NodeJS" />
-                <Badge badgeColor="black" textColor="lightgray" text="Vulkan" />
-                <Badge badgeColor="black" textColor="lightgray" text="OpenGL" />
-                <Badge badgeColor="black" textColor="lightgray" text="Sequelize" />
-                <Badge badgeColor="black" textColor="lightgray" text="Prisma ORM" />
-                <Badge badgeColor="black" textColor="lightgray" text="ExpressJS" />
-                <Badge badgeColor="black" textColor="lightgray" text="CUDA" />
-            </HStack>
-            <HStack width="100%" justifyItems="center" justifyContent="center" padding="0px" gapping="2px">
-                <Badge badgeColor="black" textColor="lightgray" text="iOS" />
-                <Badge badgeColor="black" textColor="lightgray" text="Android" />
-                <Badge badgeColor="black" textColor="lightgray" text="Windows" />
-                <Badge badgeColor="black" textColor="lightgray" text="Mac" />
-                <Badge badgeColor="black" textColor="lightgray" text="Linux" />
-                <Badge badgeColor="black" textColor="lightgray" text="Web" />
-            </HStack>
-        </VStack>
+    const favIcon = (<Image width="48px" height="48px" alt="icon" url="/images/question.png" bordered />)
+
+    const hello = (<TitleText centered>Hello!</TitleText>)
+    const selfIntro = (
+        <BodyText centered>
+            My name is Michael Pingleton.  I am a software developer who is well-versed in desktop, mobile, and web development alike.
+        </BodyText>
     )
 
-    const homepageContent = (
-        <VStack justifyItems="center" width="100%" gapping="14px">
-            <TitleText centered>Hello!</TitleText>
-            <BodyText centered>My name is Michael Pingleton. I am a software developer who is well versed in web, desktop, and mobile development alike.</BodyText>
-            <BodyText centered>I am familiar with several different programming languages, technologies, and platforms:</BodyText>
-            {badgeContent}
-            <BodyText centered>On this site, you will be able to see some of the projects that I am working on by clicking the 'Projects' tab.</BodyText>
-        </VStack>
+    const siteIntro = (
+        <BodyText centered>
+            I have created this website to showcase the projects that I have been working on.
+            Here, you will see the various software projects and 3D prints that I am working on, as well as the general tinkering that I tend to do.
+        </BodyText>
+    )
+
+    const languages = ["C", "C++", "Java", "Kotlin", "Swift", "JavaScript", "TypeScript"]
+    const platforms = ["Linux", "macOS", "Windows", "Android", "iOS", "Web"]
+    const libraries = ["Vulkan", "OpenGL", "CUDA", "React", "MaterialUI", "ExpressJS", "Prisma ORM"]
+
+    const helloPanel = (
+        <Panel>
+            <BrowserView>
+                <HStack justifyItems="center" justifyContent="center">
+                    <ProfileImage isMobile={false} />
+                    <VStack width="100%" justifyItems="center" justifyContent="center" gapping="30px">
+                        {hello}
+                        {selfIntro}
+                        {siteIntro}
+                    </VStack>
+                </HStack>
+            </BrowserView>
+            <MobileView>
+                <VStack gapping="30px">
+                    {hello}
+                    <ProfileImage isMobile={true} />
+                    {selfIntro}
+                    {siteIntro}
+                </VStack>
+            </MobileView>
+        </Panel>
+    )
+
+    const linkPanel = (
+        <SectionPanel title="Links">
+            <VStack padding="0px">
+                <SocialMediaPanelButton
+                    icon={favIcon}
+                    platform="LinkedIn"
+                    onClicc={() => {}}
+                />
+                <SocialMediaPanelButton
+                    icon={favIcon}
+                    platform="The Bit Whisperer - Substack"
+                    username="@mnpingleton"
+                    onClicc={() => {}}
+                />
+                <SocialMediaPanelButton
+                    icon={favIcon}
+                    platform="Bluesky"
+                    username="@mnpingleton.bsky.social"
+                    onClicc={() => {}}
+                />
+                <SocialMediaPanelButton
+                    icon={favIcon}
+                    platform="Instagram"
+                    username="@pinglesaurus"
+                    onClicc={() => {}}
+                />
+            </VStack>
+        </SectionPanel>
+    )
+
+    const stackPanel = (
+        <SectionPanel title="Tech Stack">
+            <VStack>
+                <BodyText centered>Programming Languages</BodyText>
+                <HStack wrap gapping="2px" padding="0px" justifyItems="center" justifyContent="center">
+                    {languages.map((l) => (<Badge badgeColor="black" textColor="lightgray" text={l} />))}
+                </HStack>
+                <HDivider />
+                <BodyText centered>Platforms</BodyText>
+                <HStack wrap gapping="2px" padding="0px" justifyItems="center" justifyContent="center">
+                    {platforms.map((l) => (<Badge badgeColor="black" textColor="lightgray" text={l} />))}
+                </HStack>
+                <HDivider />
+                <BodyText centered>Frameworks & Libraries</BodyText>
+                <HStack wrap gapping="2px" padding="0px" justifyItems="center" justifyContent="center">
+                    {libraries.map((l) => (<Badge badgeColor="black" textColor="lightgray" text={l} />))}
+                </HStack>
+            </VStack>
+        </SectionPanel>
+    )
+
+    const galleryPanel = (
+        <SectionPanel title="Gallery">
+            <BodyText centered>Under construction</BodyText>
+        </SectionPanel>
+    )
+
+    const updatePanel = (
+        <SectionPanel title="Latest Updates">
+            <BodyText centered>Under construction</BodyText>
+        </SectionPanel>
     )
 
     return (
         <AutomaticLayout navId="home" title="Home">
-            <VStack>
-                <Panel>
-                    <XStack vertical={isMobile} horizontal={isBrowser}>
-                        <ProfileImage isMobile={isMobile} />
-                        {homepageContent}
-                    </XStack>
-                </Panel>
-            </VStack>
+            <BrowserView>
+                <VStack>
+                    <HStack padding="0px">
+                        <Box width="600px">{linkPanel}</Box>
+                        <Box width="80%">{helloPanel}</Box>
+                        <Box width="50%">{stackPanel}</Box>
+                    </HStack>
+                    <HStack padding="0px">
+                        {updatePanel}
+                        <Box width="50%">{galleryPanel}</Box>
+                    </HStack>
+                    <FooterPanel />
+                </VStack>
+            </BrowserView>
+            <MobileView>
+                <VStack>
+                    {helloPanel}
+                    {linkPanel}
+                    {stackPanel}
+                    {updatePanel}
+                    {galleryPanel}
+                    <FooterPanel />
+                </VStack>
+            </MobileView>
         </AutomaticLayout>
     )
 }
