2d09771f1127c098066a6520b14e4d50cbe9711c
Michael Pingleton <mnpingleton@gmail.com>
2025-02-05 10:16:16 -0500

    Finished home page social media page redirects.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
-import { BrowserView, MobileView } from "react-device-detect"
+import { useNavigate } from "react-router-dom"
+import { isMobile, BrowserView, MobileView } from "react-device-detect"
 
 import AutomaticLayout from "../../common/layouts/AutomaticLayout"
 
@@ -15,6 +16,7 @@ import ProfileImage from "../../common/components/molecular/ProfileImage"
 import SectionPanel from "../../common/components/molecular/SectionPanel"
 import SocialMediaPanelButton from "../../common/components/molecular/SocialMediaPanelButton"
 import TechStackPanel from "../../common/components/molecular/TechStackPanel"
+import RedirectModal from "../../common/components/molecular/RedirectModal"
 
 import DataContext from "../../classes/DataContext"
 import Language from "../../classes/Language"
@@ -27,11 +29,28 @@ import getAllPlatforms from "../../data/getAllPlatforms"
 import getAllFrameworks from "../../data/getAllFrameworks"
 
 export default function Home() {
+    const navigate = useNavigate()
+
+    const [isRedirectModalOpen, setRedirectModalOpen] = useState<boolean>(false)
+    const [infoRedirectModalName, setRedirectModalName] = useState<string | undefined>(undefined)
+    const [infoRedirectModalIcon, setRedirectModalIcon] = useState<React.ReactNode | undefined>(undefined)
+    const [infoRedirectModalUrl, setRedirectModalUrl] = useState<string | undefined>(undefined)
 
     const [ctx, setCtx] = useState<DataContext>(new DataContext())
     const [languages, setLanguages] = useState<Language[] | undefined>(undefined)
     const [platforms, setPlatforms] = useState<Platform[] | undefined>(undefined)
     const [frameworks, setFrameworks] = useState<Framework[] | undefined>(undefined)
+
+    function setRedirectModal(name: string, icon: React.ReactNode, url: string) {
+        setRedirectModalName(name)
+        setRedirectModalIcon(icon)
+        setRedirectModalUrl(url)
+        setRedirectModalOpen(true)
+    }
+
+    function executeRedirect() {
+        navigate(infoRedirectModalUrl!)
+    }
 
     useEffect(() => {
         if (!ctx.isInitialized) {
@@ -57,11 +76,11 @@ export default function Home() {
         return (<AutomaticLayout loading navId="home" title="Home" />)
     }
 
-    const favIcon = (<Image width="48px" height="48px" alt="icon" url="/images/question.png" bordered />)
-    const instagramIcon = (<Image width="48px" height="48px" alt="icon" url="/images/instagram.png" />)
-    const linkedinIcon = (<Image height="48px" alt="icon" url="/images/linkedin.png" />)
-    const substackIcon = (<Image width="48px" height="48px" alt="icon" url="/images/thebitwhisperer.png" bordered />)
-    const githubIcon = (<Image width="48px" height="48px" alt="icon" url="/images/github.png" backing bordered />)
+    const favIcon = (<Image width="48px" height="48px" alt="icon" url="/images/question.png" bordered centered />)
+    const instagramIcon = (<Image width="48px" height="48px" alt="icon" url="/images/instagram.png" centered />)
+    const linkedinIcon = (<Image height="48px" alt="icon" url="/images/linkedin.png" centered />)
+    const substackIcon = (<Image width="48px" height="48px" alt="icon" url="/images/thebitwhisperer.png" bordered centered />)
+    const githubIcon = (<Image width="48px" height="48px" alt="icon" url="/images/github.png" backing bordered centered />)
 
     const hello = (<TitleText centered>Hello!</TitleText>)
     const selfIntro = (
@@ -106,31 +125,31 @@ export default function Home() {
                 <SocialMediaPanelButton
                     icon={linkedinIcon}
                     platform="LinkedIn"
-                    onClicc={() => {}}
+                    onClicc={() => setRedirectModal("Linkedin", linkedinIcon, "//www.linkedin.com/in/michael-pingleton-8833a0318")}
                 />
                 <SocialMediaPanelButton
                     icon={substackIcon}
                     platform="Substack"
                     username="The Bit Whisperer (@mnpingleton)"
-                    onClicc={() => {}}
+                    onClicc={() => setRedirectModal("Substack", substackIcon, "//mnpingleton.substack.com/")}
                 />
                 <SocialMediaPanelButton
                     icon={githubIcon}
                     platform="GitHub"
                     username="@mpingleton"
-                    onClicc={() => {}}
+                    onClicc={() => setRedirectModal("Github", githubIcon, "//github.com/mpingleton")}
                 />
                 <SocialMediaPanelButton
                     icon={favIcon}
                     platform="Bluesky"
                     username="@mnpingleton.bsky.social"
-                    onClicc={() => {}}
+                    onClicc={() => setRedirectModal("Bluesky", favIcon, "//bsky.app/profile/mnpingleton.bsky.social")}
                 />
                 <SocialMediaPanelButton
                     icon={instagramIcon}
                     platform="Instagram"
                     username="@pinglesaurus"
-                    onClicc={() => {}}
+                    onClicc={() => setRedirectModal("Instagram", instagramIcon, "//www.instagram.com/pinglesaurus")}
                 />
             </VStack>
         </SectionPanel>
@@ -216,12 +235,27 @@ export default function Home() {
     {galleryPanel}
     */
 
+    const modals = [
+        (
+            <RedirectModal
+                isOpen={isRedirectModalOpen}
+                onClose={() => setRedirectModalOpen(false)}
+                onProceed={() => executeRedirect()}
+                isMobile={isMobile}
+                siteName={infoRedirectModalName}
+                siteIcon={infoRedirectModalIcon}
+                siteUrl={infoRedirectModalUrl}
+            />
+        )
+    ]
+
     return (
         <AutomaticLayout
             navId="home"
             title="Home"
             desktopLayout={desktopLayout}
             mobileLayout={mobileLayout}
+            modals={modals}
         />
     )
 }
