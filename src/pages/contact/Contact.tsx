08860392ad03334f175427d1da950a9bf3feb1ef
Michael Pingleton <mnpingleton@gmail.com>
2025-02-10 13:27:56 -0500

    Added Contact page.

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,12 +1,29 @@
 import React from "react"
 
-import AutomaticLayout from "../../common/layouts/AutomaticLayout"
+import VStack from "../../common/components/atomic/VStack"
+import BodyText from "../../common/components/atomic/BodyText"
 
-import UnderConstructionPanel from "../../common/components/molecular/UnderConstructionPanel"
+import SectionPanel from "../../common/components/molecular/SectionPanel"
+
+import AutomaticLayout from "../../common/layouts/AutomaticLayout"
 
 export default function Contact() {
 
-    const desktopLayout = (<UnderConstructionPanel />)
+    const emailIntro1 = "If you wish to inquire about a project, report a bug, make a contribution, or suggest ideas, you may reach out to me at the following email address:"
+    const emailIntro2 = "Additionally, when reporting bugs please include details about you encountered the bug and how to reproduce it."
+    const emailAddress = "mnpingleton@gmail.com"
+
+    const contactPanel = (
+        <SectionPanel title="Contact Me">
+            <VStack gapping="30px">
+                <BodyText centered>{emailIntro1}</BodyText>
+                <BodyText centered>{emailAddress}</BodyText>
+                <BodyText centered>{emailIntro2}</BodyText>
+            </VStack>
+        </SectionPanel>
+    )
+
+    const desktopLayout = (<VStack>{contactPanel}</VStack>)
 
     return (
         <AutomaticLayout
