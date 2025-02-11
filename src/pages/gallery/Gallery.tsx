9677611a3c55b5dc82f0badf691f82c9e104ff15
Michael Pingleton <mnpingleton@gmail.com>
2025-02-10 20:30:26 -0500

    Added "Nothing to Display" messages for Gallery and Prints sections.

--- src/pages/gallery/Gallery.tsx
+++ src/pages/gallery/Gallery.tsx
@@ -2,11 +2,19 @@ import React from "react"
 
 import AutomaticLayout from "../../common/layouts/AutomaticLayout"
 
-import UnderConstructionPanel from "../../common/components/molecular/UnderConstructionPanel"
+import BodyText from "../../common/components/atomic/BodyText"
+import VStack from "../../common/components/atomic/VStack"
+import SectionPanel from "../../common/components/molecular/SectionPanel"
 
 export default function Gallery() {
 
-    const desktopLayout= (<UnderConstructionPanel />)
+    const desktopLayout= (
+        <VStack>
+            <SectionPanel title="Gallery">
+                <BodyText centered>There are currently no photos to display.  Please check back here later.</BodyText>
+            </SectionPanel>
+        </VStack>
+    )
 
     return (
         <AutomaticLayout
--- src/pages/prints/Prints.tsx
+++ src/pages/prints/Prints.tsx
@@ -2,11 +2,19 @@ import React from "react"
 
 import AutomaticLayout from "../../common/layouts/AutomaticLayout"
 
-import UnderConstructionPanel from "../../common/components/molecular/UnderConstructionPanel"
+import BodyText from "../../common/components/atomic/BodyText"
+import VStack from "../../common/components/atomic/VStack"
+import SectionPanel from "../../common/components/molecular/SectionPanel"
 
 export default function Prints() {
 
-    const desktopLayout = (<UnderConstructionPanel />)
+    const desktopLayout = (
+        <VStack>
+            <SectionPanel title="3D Printing">
+                <BodyText centered>There are currently no 3D printing projects to display.  Please check back here later.</BodyText>
+            </SectionPanel>
+        </VStack>
+    )
 
     return (
         <AutomaticLayout
