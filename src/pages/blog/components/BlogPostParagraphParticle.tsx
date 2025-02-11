f3ddc369d836456734740789e67ac6db24e18cee
Michael Pingleton <mnpingleton@gmail.com>
2025-02-10 20:30:45 -0500

    Tweaked formatting of paragraphs for blog posts.

--- src/pages/blog/components/BlogPostParagraphParticle.tsx
+++ src/pages/blog/components/BlogPostParagraphParticle.tsx
@@ -1,10 +1,18 @@
 import React from "react"
 
+import Box from "../../../common/components/atomic/Box"
+import VStack from "../../../common/components/atomic/VStack"
 import BodyText from "../../../common/components/atomic/BodyText"
 
 import BlogParagraph from "../../../classes/BlogParagraph"
 
 export default function BlogPostParagraphParticle(props: { data: BlogParagraph }) {
 
-    return (<BodyText>{props.data.text}</BodyText>)
+    return (
+        <VStack>
+            <Box width="98%">
+                <BodyText>{props.data.text}</BodyText>
+            </Box>
+        </VStack>
+    )
 }
